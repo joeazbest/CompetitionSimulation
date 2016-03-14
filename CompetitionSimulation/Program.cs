e5e4d994cd8ae7c5feffecd5818378d9b4c07366
Martin Kudrna <kudrna.m@gmail.com>
2016-03-14 16:41:29 +0100

    drobnosti

--- CompetitionSimulation/Program.cs
+++ CompetitionSimulation/Program.cs
@@ -1,11 +1,11 @@
 ﻿namespace CompetitionSimulation
 {
 	using Algorithms;
-	using Teams;
 	using System;
 	using System.Collections.Generic;
 	using System.IO;
 	using System.Linq;
+	using Teams;
 
 	public class Program
 	{
@@ -23,7 +23,7 @@
 					var value = teamOrder;  // predpokladam ze dve rnd cisla nebudou v 18ti cisle stejny :-)
 					teams.Add(
 						rnd.NextDouble(),
-						new Team($"Team {teamOrder}", x => value)
+						new Team($"{teamOrder}", x => value)
 					);
 				}
 
@@ -31,6 +31,7 @@
 				var teamInput = teams.OrderBy(t => t.Key).Select(t => t.Value).ToList();
 				var order = 1;
 				var basket = 1;
+
 				foreach (var team in teamInput)
 				{
 					output.Write("{0} ", team.Name);
@@ -44,24 +45,45 @@
 						basket++;
 					}
 				}
-				output.WriteLine();
-				output.Flush();
 
 				// projedu jednotlivy algoritmy
 				var alg1 = new NonOrganizerAlgorithm(teamInput);
 				var alg2 = new OrganizerPriorityAlgorithm(teamInput);
+				var alg3 = new DynamicOrganizerAlgorithm(teamInput);
 
 				var basket1First = alg1.CreateInitialBasket();
 				var basket2First = alg2.CreateInitialBasket();
+				var basket3First = alg3.CreateInitialBasket();
 
-				for (var round = 1; round <= 11; round++)
+				for (var round = 1; round <= 7; round++)
 				{
 					var basket1Second = alg1.GetNextBasketComposition(basket1First);
 					var basket2Second = alg2.GetNextBasketComposition(basket2First);
+					var basket3Second = alg3.GetNextBasketComposition(basket3First);
 
 					basket1First = basket1Second;
 					basket2First = basket2Second;
+					basket3First = basket3Second;
 				}
+
+				output.Write(" | ");
+				foreach (var team in alg1.GetTeamFinalOrder(basket1First))
+				{
+					output.Write("{0} ", team.Name);
+				}
+				output.Write(" | ");
+				foreach (var team in alg2.GetTeamFinalOrder(basket2First))
+				{
+					output.Write("{0} ", team.Name);
+				}
+				output.Write(" | ");
+				foreach (var team in alg3.GetTeamFinalOrder(basket3First))
+				{
+					output.Write("{0} ", team.Name);
+				}
+
+				output.WriteLine();
+				output.Flush();
 			}
 
 			output.Close();
