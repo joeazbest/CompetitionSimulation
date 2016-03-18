c323489c7c0ed33370e65a2d6c98fdf2ec241602
Martin Kudrna <kudrna.m@gmail.com>
2016-03-18 13:41:00 +0100

    drobne upravy

--- CompetitionSimulation/Program.cs
+++ CompetitionSimulation/Program.cs
@@ -9,32 +9,42 @@
 
 	public class Program
 	{
+		private static readonly int atemptCount = 10000;
+		private static readonly int teamCount = 18;
+
 		public static void Main(string[] args)
 		{
 			var rnd = new Random();
 			var output = new StreamWriter("..\\..\\..\\TxtOutput\\output.txt");
 
-			for (var i = 1; i <= 1000; i++)
+			var optimalVector = new int[teamCount];
+			var primaryVector = new int[teamCount];
+			var lastVector = new int[teamCount];
+
+			for (var i = 1; i <= atemptCount; i++)      // POCET POKUSU
 			{
 				// seznam tymu
 				var teams = new Dictionary<double, ITeam>();
-				for (var teamOrder = 1; teamOrder <= 36; teamOrder++)
+				for (var teamOrder = 1; teamOrder <= teamCount; teamOrder++)    // POCET TYMU
 				{
-					var value = teamOrder;  // predpokladam ze dve rnd cisla nebudou v 18ti cisle stejny :-)
+					var value = teamOrder;  // TODO predpokladam ze dve rnd cisla nebudou v 18ti cislech stejny :-)
 					teams.Add(
 						rnd.NextDouble(),
-						new Team($"{teamOrder}", x => value)
+						new Team($"{value}", value, x => value) // vsechno je stejny ale to nemusi byt pravda :-)
 					);
+
+					optimalVector[teamOrder - 1] = teamCount - teamOrder + 1;
 				}
 
 				// pridam poradatelstvi - kazdemu jednou
 				var teamInput = teams.OrderBy(t => t.Key).Select(t => t.Value).ToList();
 				var order = 1;
 				var basket = 1;
+				var fullOrder = 0;
 
 				foreach (var team in teamInput)
 				{
-					output.Write("{0} ", team.Name);
+					// output.Write("{0} ", team.Name);
 
 					team.AddOrganizer(new Organizer(order, basket.ToString()));
 					team.AddOrganizer(new Organizer(order + 6, basket.ToString()));
@@ -44,6 +54,9 @@
 						order = 1;
 						basket++;
 					}
+
+					primaryVector[fullOrder] = team.OptimalOrder;
+					fullOrder++;
 				}
 
 				// projedu jednotlivy algoritmy
@@ -55,7 +68,7 @@
 				var basket2First = alg2.CreateInitialBasket();
 				var basket3First = alg3.CreateInitialBasket();
 
-				for (var round = 1; round <= 7; round++)
+				for (var round = 1; round <= 7; round++)       // POCET KOL + 1
 				{
 					var basket1Second = alg1.GetNextBasketComposition(basket1First);
 					var basket2Second = alg2.GetNextBasketComposition(basket2First);
@@ -66,27 +79,80 @@
 					basket3First = basket3Second;
 				}
 
-				output.Write(" | ");
-				foreach (var team in alg1.GetTeamFinalOrder(basket1First))
-				{
-					output.Write("{0} ", team.Name);
-				}
-				output.Write(" | ");
-				foreach (var team in alg2.GetTeamFinalOrder(basket2First))
-				{
-					output.Write("{0} ", team.Name);
-				}
-				output.Write(" | ");
-				foreach (var team in alg3.GetTeamFinalOrder(basket3First))
-				{
-					output.Write("{0} ", team.Name);
-				}
+				output.Write("{0}\t", VectorDiff(optimalVector, primaryVector));
+
+				FillLastVector(
+					lastVector,
+					alg1.GetTeamFinalOrder(basket1First)
+				);
+				output.Write("{0}\t", VectorDiff(optimalVector, lastVector));
+
+				FillLastVector(
+					lastVector,
+					alg2.GetTeamFinalOrder(basket2First)
+				);
+				output.Write("{0}\t", VectorDiff(optimalVector, lastVector));
+
+				FillLastVector(
+					lastVector,
+					alg3.GetTeamFinalOrder(basket3First)
+				);
+				output.Write("{0}\t", VectorDiff(optimalVector, lastVector));
+
+				//output.Write(" | ");
+				//foreach (var team in alg1.GetTeamFinalOrder(basket1First))
+				//{
+				//	output.Write("{0} ", team.Name);
+				//}
+				//output.Write(" | ");
+				//foreach (var team in alg2.GetTeamFinalOrder(basket2First))
+				//{
+				//	output.Write("{0} ", team.Name);
+				//}
+				//output.Write(" | ");
+				//foreach (var team in alg3.GetTeamFinalOrder(basket3First))
+				//{
+				//	output.Write("{0} ", team.Name);
+				//}
 
 				output.WriteLine();
 				output.Flush();
 			}
 
 			output.Close();
+		}
+
+		private static void FillLastVector(
+			int[] outputVector,
+			IList<ITeam> teamOutput
+		)
+		{
+			var lastOrder = 0;
+			foreach (var team in teamOutput)
+			{
+				outputVector[lastOrder] = team.OptimalOrder;
+				lastOrder++;
+			}
+		}
+
+		private static double VectorDiff(
+			int[] vector1,
+			int[] vector2
+		)
+		{
+			if (vector1 == null || vector2 == null)
+				throw new ArgumentNullException("Vector");
+
+			if (vector1.Count() != vector2.Count())
+				throw new ArgumentException("Different vector count");
+
+			var output = 0d;
+			for (var i = 0; i < vector1.Count(); i++)
+			{
+				var diff = vector1[i] - vector2[i];
+				output +=Math.Abs(diff * diff * diff);
+			}
+			return Math.Sqrt(output);
 		}
 	}
 }
